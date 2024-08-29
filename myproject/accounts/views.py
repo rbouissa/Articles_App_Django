534a81d2429626004fb298c9f3765672a3f6791d
rbouissa <redabouissali422@gmail.com>
2024-08-29 13:35:08 +0100

    dsdssd

---
+++ myproject/accounts/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib import messages
+from django.urls import reverse
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "User created successfully!")
+            return redirect(reverse('article_list'))
+        else :
+             messages.success(request, "User created successfully!")
+    else:
+        form = UserCreationForm()
+    return render(request,'accounts/signup.html',{'form':form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None : 
+                auth_login(request, user)
+                return redirect (reverse('article_list'))
+            else:
+                messages.error(request, "Invalid username or password.")
+
+    else:
+        form = AuthenticationForm()
+    return render(request,'accounts/login.html',{'form':form})
+
