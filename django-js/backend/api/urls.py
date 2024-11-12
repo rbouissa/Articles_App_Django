3d4b8f89f7dcdc4700b4ba0a8bab3396c4804982
rbouissa <redabouissali422@gmail.com>
2024-11-12 10:47:57 +0100

    SDsds

--- django-js/backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import SampleAPI
-from .views import Signup
-from .views import Login
-urlpatterns = [
-    # path('sample/', SampleAPI.as_view(), name='sample_api'),
-    path('signup/', Signup.as_view(), name='signup'),
-    path('login/',Login.as_view(),name='login'),
-]
